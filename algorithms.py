b0a50affd2ca0cee8f1b1b8bdd1f323ece7d4a16
Sam <samuelgardner101@gmail.com>
2024-02-27 13:12:40 +0000

    Remove unused algorithms

--- algorithms.py
+++ algorithms.py
@@ -2,7 +2,6 @@ ALGORITHMS = {
     "kyber1024": {"path": "kyber1024", "is_kem": True},
     "kyber512": {"path": "kyber512", "is_kem": True},
     "kyber768": {"path": "kyber768", "is_kem": True},
-    "lightsaber": {"path": "lightsaber", "is_kem": True},
     "dilithium2": {"path": "dilithium2", "is_sign": True},
     "dilithium3": {"path": "dilithium3", "is_sign": True},
     "dilithium4": {"path": "dilithium4", "is_sign": True},
